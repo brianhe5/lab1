c11b3ef8bcb3faca012d798d1e6fb4dd141d011a
Brian He <brianhek123@gmail.com>
2024-02-02 21:03:04 -0800

    asdf

--- pipe.c
+++ pipe.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
         	perror("fork");
         	exit(errno);
 		}
-		fprintf(stdout, "child id: %d\n", child_pid);
+		fprintf(stdout, "child id!: %d\n", child_pid);
 
 		fprintf(stdout, "child id: %d, iteration: %d\n", child_pid,  i);
 		fprintf(stdout, "testing");
